5696384f45e4edb9f2cfe1e99044fd0000e623f3
Partha Das <parthodasm@gmail.com>
2026-02-18 18:32:41 +0600

    fix server side socketio issues

--- server/index.js
+++ server/index.js
@@ -37,7 +37,10 @@ app.use(cookieParser());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://code-collab-delta-umber.vercel.app",
+    ],
     credentials: true,
   },
 });
